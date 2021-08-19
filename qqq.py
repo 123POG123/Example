8b20fcef188c26d827d14a194b63594ce38ab359
Pavel <45313368+123POG123@users.noreply.github.com>
2021-08-19 07:02:32 +0300

    '19.08'

--- qqq.py
+++ qqq.py
@@ -1,4 +1,8 @@
-a = ['wwww','12']
-b = ''
-b.join(a)
-print(b)
+stores = [
+    {'name': 'Downtown', 'street': '385 Main Street', 'city': 'San Diego'},
+    {'name': 'Uptown', 'street': '231 Highland Avenue', 'city': 'San Diego'},
+    {'name': 'Midtown', 'street': '85 Balboa Street', 'city': 'San Diego'},
+    {'name': 'Downtown', 'street': '639 Spring Street', 'city': 'Los Angeles'},
+    {'name': 'Midtown', 'street': '1407 Broadway Street', 'city': 'Los Angeles'},
+    {'name': 'Downton', 'street': '50 1st Street', 'city': 'San Francisco'},
+]
